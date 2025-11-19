7995b0d20d72dc869ea945f520139cd287a9b82f
Robin Lugo Boero <rjlb991102@hotmail.com>
2025-11-19 16:12:20 -0500

    fix: Permisos de acceso a la documentacón Swagger

--- src/main/java/com/robinlb99/legalserviceportalapi/config/SecurityConfig.java
+++ src/main/java/com/robinlb99/legalserviceportalapi/config/SecurityConfig.java
@@ -63,6 +63,7 @@ public class SecurityConfig {
         http
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/api/registrarse/**").permitAll()
+                    .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                     .anyRequest().authenticated()
             )
             .httpBasic(Customizer.withDefaults())
