5787568901b5a6aa9c96dbd6cdff25521a211456
Robin Lugo Boero <rjlb991102@hotmail.com>
2025-11-19 23:02:54 -0500

    docs: Se documenta

--- src/main/java/com/robinlb99/legalserviceportalapi/config/OpenApiConfig.java
+++ src/main/java/com/robinlb99/legalserviceportalapi/config/OpenApiConfig.java
@@ -6,9 +6,17 @@ import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuración de OpenAPI para la documentación de la API.
+ */
 @Configuration
 public class OpenApiConfig {
 
+    /**
+     * Configura y personaliza la instancia de OpenAPI.
+     *
+     * @return una instancia de {@link OpenAPI} con la información de la API.
+     */
     @Bean
     public OpenAPI CustomOpenAPI() {
         return new OpenAPI().info(
